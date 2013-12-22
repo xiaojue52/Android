1b6277077cd875bf0158a09b85a7db8363c84078
hqwu <464693894@qq.com>
2013-12-22 16:55:10 +0800

    2013/12/22 16:55

--- Android1219/src/com/example/android1219/Thread/TestThreadActivity.java
+++ Android1219/src/com/example/android1219/Thread/TestThreadActivity.java
@@ -5,6 +5,9 @@ import com.example.android1219.R;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -13,8 +16,16 @@ import android.widget.Toast;
 
 public class TestThreadActivity extends Activity {
 	private Context context;
-	private Button test_updateUI;
+	private Button test_updateUI,test_handler,test_newClass;
 	private TextView test_updateUI_text;
+	private Handler handler=new Handler(){
+		@Override
+		public void handleMessage(Message msg){
+			if (msg.what==3){
+				Toast.makeText(context, "i sleeped 3 s", Toast.LENGTH_LONG).show();
+			}
+		}
+	};
 	public TestThreadActivity() {
 		// TODO Auto-generated constructor stub
 		context = this;
@@ -23,10 +34,55 @@ public class TestThreadActivity extends Activity {
 	protected void onCreate(Bundle saveBundle){
 		super.onCreate(saveBundle);
 		this.setContentView(R.layout.thread_activity);
+
 		this.test_updateUI = (Button)this.findViewById(R.id.test_updateUI);
 		this.test_updateUI_text = (TextView)this.findViewById(R.id.test_updateUI_text);
 		this.test_updateUI.setOnClickListener(Test_UpdateUI);
+		this.test_handler = (Button)this.findViewById(R.id.test_handler);
+		this.test_handler.setOnClickListener(Test_Handler);
+		this.test_newClass = (Button)this.findViewById(R.id.test_newClass);
+		this.test_newClass.setOnClickListener(Test_NewClass);
 	}
+	private OnClickListener Test_NewClass = new OnClickListener(){
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Test test = new Test(){
+				@Override
+				public void run(){
+					Toast.makeText(context, "this is override method", Toast.LENGTH_LONG).show();
+				}
+			};
+			test.run2();
+		}
+		
+	};
+	private OnClickListener Test_Handler = new OnClickListener(){
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Runnable run = new Runnable(){
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					handler.sendEmptyMessage(3);
+				}
+				
+			};
+			Thread thread = new Thread(run);
+			thread.start();
+		}
+		
+	};
 	private OnClickListener Test_UpdateUI = new OnClickListener(){
 
 		@Override
@@ -89,5 +145,16 @@ public class TestThreadActivity extends Activity {
 	@Override
 	protected void onDestroy(){
 		super.onDestroy();
+	}
+	class Test{
+		public Test(){
+			this.run();
+		}
+		public void run(){
+			Log.v("Test","parent");
+		}
+		public void run2(){
+			Toast.makeText(context, "parent method", Toast.LENGTH_LONG).show();
+		}
 	}
 }
--- Android1222/src/com/example/android1222/MainActivity.java
+++ Android1222/src/com/example/android1222/MainActivity.java
@@ -2,16 +2,35 @@ package com.example.android1222;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	private Context context;
+	private Button call_service;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		context = this;
+		call_service = (Button)this.findViewById(R.id.call_service);
+		this.call_service.setOnClickListener(Call_Service);
 	}
 
+	private OnClickListener Call_Service = new OnClickListener(){
+
+		@Override
+		public void onClick(View arg0) {
+			// TODO Auto-generated method stub
+			Intent it = new Intent("com.example.android1219.Service.MyLocalService");
+			startService(it);
+		}
+		
+	};
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
